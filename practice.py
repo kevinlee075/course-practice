0a59ecc51ad349f5e7af604eac26c86f7af753a1
kevinlee075 <kevinlee075@gmail.com>
2021-06-13 22:28:07 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -5,24 +5,22 @@ Created on Fri Jun 11 19:47:10 2021
 @author: Lenovo
 """
 
-print('Please think of a number between 0 and 100!')
-x = 100
-low = 0
-high = x
-ans = int((low + high)/2)
-while abs(ans - x) >= 0:
-    print('Is your secret number ' + str(ans) + '?')
-    print("Enter 'h' to indicate the guess is too high. \
-Enter 'l' to indicate the guess is too low. \
-              Enter 'c' to indicate I guessed correctly.", end=' ')
-    s = str(input(''))
-    if s == 'h':
-        high = ans
-    elif s == 'l':
-        low = ans
-    elif s == 'c':
-        break
+def isIn(char, aStr):
+    '''
+    char: a single character
+    aStr: an alphabetized string
+    
+    returns: True if char is in aStr; False otherwise
+    '''
+    mid_index = len(aStr)//2
+    mid = aStr[mid_index]
+    if char == mid:
+        return True
+    elif char < mid:
+        x = aStr[:mid_index+1]
+        return isIn(char, x)
+    elif char > mid:
+        x = aStr[mid_index+1:]
+        return isIn(char, x)
     else:
-        print('Sorry, I did not understand your input.')
-    ans = int((low + high)/2)
-print('Game over. Your secret number was: ' + str(ans))
+        return False
