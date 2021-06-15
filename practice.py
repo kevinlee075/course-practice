0e6033d13e17435beb8a59d123788a7a415d1b60
kevinlee075 <kevinlee075@gmail.com>
2021-06-15 20:58:34 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -5,18 +5,18 @@ Created on Fri Jun 11 19:47:10 2021
 @author: Lenovo
 """
 
-import math
-n = int(input('Number of sides = '))
-s = float(input('length of polygon = '))
-def tan_1(n):
+import math #To use tangent function, I need to first import the library "math"
+n = int(input('Number of sides = ')) #define the number of sides in polygon
+s = float(input('length of side = ')) #define the length of every side in polygon
+def tan_1(n): #define tan_1(n) as tan(pi/n)
     return math.tan(math.pi/n)
-def area_polygon(n, s):
+def area_polygon(n, s): #define the area of polygon based on the formula in the problem description
     return 0.25 * n * s * s / tan_1(n)
 def perimeter_polygon(n, s):
-    return s * n
-def polysum(n, s):
+    return s * n #perimeter = s plus s totally n times
+def polysum(n, s): #polysum = area + perimeter^2
     polysum = area_polygon(n, s) + perimeter_polygon(n, s) * perimeter_polygon(n, s)
-    return round(polysum, 4)
+    return round(polysum, 4) #make the answer round to 4 decimal places
 
 
     
